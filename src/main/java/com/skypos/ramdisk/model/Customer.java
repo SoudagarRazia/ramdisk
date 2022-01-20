c10ea084dd8f225168c1d92fd3b9d9b6081c292e
SoudagarRazia <raziasultana14@gmail.com>
2022-01-20 20:07:22 +0530

    Update Customer.java
    Generic Generator to main Identity in table

--- src/main/java/com/skypos/ramdisk/model/Customer.java
+++ src/main/java/com/skypos/ramdisk/model/Customer.java
@@ -1,6 +1,7 @@
 package com.skypos.ramdisk.model;
 
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -16,7 +17,9 @@ public class Customer {
     @Id
    /* @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seqGen")
     @SequenceGenerator(name = "seqGen",sequenceName = "customer_id_seq",initialValue = 1,allocationSize = 1)*/
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    //@GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(generator = "userGen")
+    @GenericGenerator(name="userGen",strategy = "increment")
     @Column(name = "customer_id")
     private Long id;
     @Column(name= "first_name")
